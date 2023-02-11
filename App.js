1d9c63406a474c7b4124625010783c614708fbdb
shipra <fshipra@seattleu.edu>
2023-02-10 18:15:26 -0800

    added post request

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 const express = require('express');
-const app = express();
-const bodyparser = require("body-parser");
-const PORT = process.env.PORT || 8080;
+const sharp = require('sharp');
+const bodyparser = require('body-parser');
+const multer = require('multer');
 
+const PORT = process.env.PORT || 8080;
+const app = express();
+const upload = multer;
+
+app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.get('/home', (req, res) => {
     res.send("Image processor")
 });
+
+app.post('/newPost', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    res.send({
+        "name": name,
+        "age": age,
+        "status": "ok"
+    });
+});
+
+// app.post('/upload', upload.single('image'), (req, res) => {
+//     sharp(req.file.buffer)
+//       .resize(500)
+//       .toBuffer()
+//       .then(data => {
+//         res.set('Content-Type', 'image/jpeg');
+//         res.send(data);
+//       })
+//       .catch(error => {
+//         res.status(500).send(error.message);
+//       });
+// });
+  
 
 app.listen(PORT, ()=>{
     console.log(`Server is up and running on port ${PORT}`);
