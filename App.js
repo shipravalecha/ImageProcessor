5d6eeb80f211e1df836fab66f182b7c52175693a
shipra <fshipra@seattleu.edu>
2023-02-12 23:07:29 -0800

    Added swagger documentation

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sharp = require('sharp');
 const bodyparser = require('body-parser');
 const multer = require('multer');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require("./swagger")
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -9,6 +11,7 @@ const upload = multer;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/home', (req, res) => {
     res.send("Image processor")
@@ -24,19 +27,18 @@ app.post('/newPost', (req, res) => {
     });
 });
 
-// app.post('/upload', upload.single('image'), (req, res) => {
-//     sharp(req.file.buffer)
-//       .resize(500)
-//       .toBuffer()
-//       .then(data => {
-//         res.set('Content-Type', 'image/jpeg');
-//         res.send(data);
-//       })
-//       .catch(error => {
-//         res.status(500).send(error.message);
-//       });
-// });
-  
+app.post('/upload', (req, res) => {
+    // sharp(req.file.buffer)
+    //   .resize(500)
+    //   .toBuffer()
+    //   .then(data => {
+    //     res.set('Content-Type', 'image/jpeg');
+    //     res.send(data);
+    //   })
+    //   .catch(error => {
+    //     res.status(500).send(error.message);
+    //   });
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is up and running on port ${PORT}`);
